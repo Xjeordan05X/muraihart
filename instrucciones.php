7007ec8a3f109dd0746a088143025d955ffe037d
Xjeordan05X <84251964+Xjeordan05X@users.noreply.github.com>
2021-05-15 21:33:13 -0500

    Update instrucciones.php

--- instrucciones.php
+++ instrucciones.php
@@ -6,6 +6,6 @@ $telefono= $_POST["telefono"];
 $pregunta = $_POST["pregunta"];
 $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " . $telefono . "\nPregunta: " . $pregunta;
 mail($destino, "Contacto", $contenido);
-header("gracias.html");
+header("location:gracias.html");
 /* header("carteles1.html"); en esta parte se pone la direccion de un nuevo html donde se den las gracias y un boton volver a la  */
 ?>
